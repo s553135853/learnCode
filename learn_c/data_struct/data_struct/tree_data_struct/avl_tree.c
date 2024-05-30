eedd43b429780708a2b34864ea7a23b3fed1a114
shenglin <shenglin24s@163.com>
2024-05-30 09:40:38 +0800

    增加题目描述以及解题思路

--- learn_c/data_struct/data_struct/tree_data_struct/avl_tree.c
+++ learn_c/data_struct/data_struct/tree_data_struct/avl_tree.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * 题目描述：
+ * 有一个二叉搜索树因为有两个节点更换了位置导致不是二叉搜索树了,要求恢复这个二叉搜索树 
+ * 
+ * 
+ * 
+ * 恢复二叉搜索树思路:
+ * 1、中序遍历二叉树得到一个序列存到结构体数组里面
+ * 2、寻找逆序对,如果只有一个逆序对,则只需要更换相邻节点即可
+ * 3、如果存在两个逆序对,则需要更换第一个逆序对的第一个节点和第二个逆序对的第二个节点
+*/
+
+
 struct binary_tree {
     int data;
     struct binary_tree *rch, *lch;
@@ -48,7 +61,6 @@ void mid_array_node(struct TreeNode *root, struct TreeNode_temp *p)
 void find_err_node(struct TreeNode *root, struct TreeNode_temp *p)
 {
     for (size_t i = 0; i < count - 1; i++) {
-#if 1
         if (p[i + 1].val < p[i].val) {
             if (index1 == -1) {
                 index1 = i;
@@ -59,7 +71,6 @@ void find_err_node(struct TreeNode *root, struct TreeNode_temp *p)
                 break;
             }
         }
-#endif
     }
 
     if (index1 != -1 && index2 != -1) {
@@ -68,13 +79,11 @@ void find_err_node(struct TreeNode *root, struct TreeNode_temp *p)
         p[index2].node->val = tes;
     } else {
         if (index1 != -1) {
-
             int tes = p[index1].node->val;
             p[index1].node->val = p[index1 + 1].node->val;
             p[index1 + 1].node->val = tes;
 
         } else {
-
             int tes = p[index1].node->val;
             p[index1].node->val = p[index1 + 1].node->val;
             p[index1 + 1].node->val = tes;
@@ -84,7 +93,6 @@ void find_err_node(struct TreeNode *root, struct TreeNode_temp *p)
 
 void recoverTree(struct TreeNode *root)
 {
-
     mid_count_node(root);
     struct TreeNode_temp *p = (struct TreeNode_temp *)malloc(sizeof(struct TreeNode_temp) * count);
     count = 0;
